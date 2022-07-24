5b00d7f3de76076bce8ef4383d302a67b312fb5c
Pixlz <91509874+darkpixlz@users.noreply.github.com>
2022-07-23 21:55:55 -0400

    Create PanelControl.lua

---
+++ PanelControl.lua
@@ -0,0 +1,17 @@
+local module = {}
+--[[
+
+
+new api lets go
+what are you doing here, this should have been hiddennnnnn
+whatever i'm getting tired 
+
+]]
+
+function module:OpenPanel(plr)
+	
+	
+	
+end
+
+return module
