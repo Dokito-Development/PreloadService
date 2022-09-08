a94417ad78a45a2375e6fd868540b7838ee2911e
Pixlz <91509874+darkpixlz@users.noreply.github.com>
2022-09-08 16:49:24 -0400

    v2.3

--- PreloadService.lua
+++ PreloadService.lua
@@ -1,11 +1,10 @@
 --[[
 PRELOADSERVICE V2
-
-The following is all essential to providing the core funstions, and exploit protection.
-
-Chances are if you have no idea what you're doing and dont know lua well you shouldn't change anything.
+The following is all essential to providing the core functions, and exploit protection.
+I advise you do not modify any code, but I cant stop you.
+IF YOU MODIFY CODE I WILL NOT OFFER SUPPORT
 ]]
-local CurrentVers = "2.2"
+local CurrentVers = "2.3"
 local adminIDs = require(script.Admins).Admins
 local players = game:GetService("Players")
 local moduleNotRequire = script.PreloadService
@@ -37,6 +36,10 @@ local PS = require(game.ReplicatedStorage.PreloadService)
 local CompletedTimes = {}
 local Decimals = 2
 local ServerLifetime = 0
+local function GetSetting(Setting)
+	local SettingModule = require(game.ReplicatedStorage.PreloadService.Settings)
+	return SettingModule[Setting]
+end
 local function Format(Int)
 	return string.format("%02i", Int)
 end
@@ -134,6 +137,7 @@ local function NewNotification(admin, bodytext, headingtext, image, dur, t)
 	task.spawn(n,admin,bodytext,headingtext,image,dur, t)
 end
 
+
 local function GetTimeWithSeconds(Seconds)
 	local Minutes = (Seconds - Seconds%60)/60;
 	Seconds = Seconds - Minutes*60;
@@ -143,6 +147,65 @@ local function GetTimeWithSeconds(Seconds)
 
 	return Format(Hours)..":"..Format(Minutes)..":"..Format(Seconds);
 end
+local CurrentlyExisting = 0
+--Thanks to @334901766 for helping out a bit
+local function MakeHomeHistoryBox(Admin,PFP,Username,InstanceName,InstanceType,Time)
+	CurrentlyExisting = #Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget:GetChildren()
+	if CurrentlyExisting >= 50 then
+		for i = 1, (CurrentlyExisting - 50) do
+			Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget:FindFirstChildWhichIsA("Frame"):Destroy()
+		end
+	end
+	CurrentlyExisting = #Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget:GetChildren()
+
+	local NewCard = Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.Template:Clone()
+	NewCard.Parent =Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget
+	NewCard.Visible = true
+	NewCard.Name = CurrentlyExisting
+	NewCard.PlayerImage.Image = PFP
+	NewCard.Username.Text = "@"..Username
+	NewCard.Time.Text = Time
+	NewCard.Type.Text = InstanceName
+end
+
+local function MakeHistory(Admin,PFP,Username,InstanceName,InstanceType,Time)
+	CurrentlyExisting = #Admin.PlayerGui.PreloadServiceAdminPanel.Main.History.MainFrame:GetChildren()
+	if CurrentlyExisting >= GetSetting("MaxHistoryItems") then
+		for i = 1, (CurrentlyExisting - GetSetting("MaxHistoryItems")) do
+			Admin.PlayerGui.PreloadServiceAdminPanel.Main.History.MainFrame:FindFirstChildWhichIsA("Frame"):Destroy()
+		end
+	end
+	CurrentlyExisting = #Admin.PlayerGui.PreloadServiceAdminPanel.Main.History.MainFrame:GetChildren()
+
+	local NewCard = Admin.PlayerGui.PreloadServiceAdminPanel.Main.History.Template:Clone()
+	NewCard.Parent =Admin.PlayerGui.PreloadServiceAdminPanel.Main.History.MainFrame
+	NewCard.Visible = true
+	NewCard.Name = CurrentlyExisting
+	NewCard.PlayerImage.Image = PFP
+	NewCard.Username.Text = "@"..Username
+	NewCard.Time.Text = Time
+	NewCard.Type.Text = InstanceName
+end
+local function MakeModuleHistoryCard(Admin,PFP,Username,InstanceName,InstanceType,Time)
+	CurrentlyExisting = #Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget:GetChildren()
+	if CurrentlyExisting >= 50 then
+		for i = 1, (CurrentlyExisting - 50) do
+			Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget:FindFirstChildWhichIsA("Frame"):Destroy()
+		end
+	end
+	CurrentlyExisting = #Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget:GetChildren()
+
+	local NewCard = Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.Template:Clone()
+	NewCard.Parent =Admin.PlayerGui.PreloadServiceAdminPanel.Main.AHome.HistoryWidget
+	NewCard.Visible = true
+	NewCard.Name = CurrentlyExisting
+	NewCard.PlayerImage.Image = PFP
+	NewCard.Username.Text = "@"..Username
+	NewCard.Time.Text = Time
+	NewCard.Type.Text = InstanceName
+end
+
+
 local function VersionCheck(plr)
 	task.wait(2)
 	if not table.find(InGameAdmins,plr) then
@@ -155,7 +218,7 @@ local function VersionCheck(plr)
 	local VersModule = require(8788148542)
 	local Frame = plr.PlayerGui.PreloadServiceAdminPanel.Main.Menu.Main.BUpdate
 	Frame.Parent.AInfo.vers.Text = CurrentVers.." by DarkPixlz, 2022".."(latest avail: "..VersModule.Version..", released "..VersModule.ReleaseDate..". Licensed under TBD."
-	if VersModule.Version ~= "2.2" then
+	if VersModule.Version ~= CurrentVers then
 		warn("[PreloadService]: Out of date! Please update your module by closing this server.")
 		Frame.Value.Value = tostring(math.random(1,100000000))
 		NewNotification(plr, "Your module is out of date. Please update your module by closing the servers, then replace it in Studio.", "Version check complete", "rbxassetid://9894144899", 10)
@@ -169,11 +232,11 @@ players.PlayerAdded:Connect(function(plr)
 		newPanel.Parent = plr.PlayerGui
 
 		if game:GetService("RunService"):IsStudio() then
-			NewNotification(plr,"Sorry, but PreloadService's Admin Panel does not operate properly in Studio. You can still enter it, but pages might not load properly. Before submitting a bug report, please make sure to check that you have the same result in the mail Roblox client.","Error!","rbxassetid://9894144899",15, true)
+			NewNotification(plr,"Sorry, but PreloadService Admin does not work in Studio. Pages do not operate and display data.","Error!","rbxassetid://9894144899",15, true)
 		else
 			task.spawn(NewNotification,plr,"Please wait, loading Admin Panel","Loading PreloadService Admin Panel v"..CurrentVers,"rbxassetid://9894144899", 8)
 		end
-		
+
 		for i, asset in pairs(newPanel:GetDescendants()) do
 			local succ, err = pcall(function()
 				game:GetService("ContentProvider"):PreloadAsync({asset})
@@ -186,10 +249,10 @@ players.PlayerAdded:Connect(function(plr)
 		local Frame = plr.PlayerGui.PreloadServiceAdminPanel.Main.Menu.Main.BUpdate
 		Frame.Parent.AInfo.vers.Text = CurrentVers.." by DarkPixlz, 2022. Licensed under TBD."
 		NewNotification(plr,"PreloadSerevice Admin Panel v"..CurrentVers.." loaded! Press F2 to enter the panel.","Welcome!","rbxassetid://10012255725",15)
-		
+
 	end
 	game:GetService("Players").PlayerRemoving:Connect(function()
-		
+
 	end)
 	local newPlrCount = #players:GetPlayers()
 	PlrCount += newPlrCount
@@ -206,68 +269,71 @@ players.PlayerRemoving:Connect(function(plr)
 		table.remove(InGameAdmins, table.find(InGameAdmins, plr))
 	end
 end)
-SpecialEvent.OnServerEvent:Connect(function(plrLoaded, Time, itmClass, itmName, ModOrRegular, item)
+SpecialEvent.OnServerEvent:Connect(function(PlayerLoaded, Time, ItemClass, ItemName, ModOrRegular, item)
 	table.insert(CompletedTimes,Time)
 	ServerLifetime += 1
 	for i, v in ipairs(InGameAdmins) do
+		MakeHomeHistoryBox(
+			v,
+			players:GetUserThumbnailAsync(PlayerLoaded.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420),
+			PlayerLoaded.Name,
+			ItemName,
+			ItemClass,
+			GetTimeWithSeconds(Time)
+		)
 		local Frame = v.PlayerGui.PreloadServiceAdminPanel.Main.History.MainFrame
 		if not Frame then
 			warn("[PreloadService ERROR]: Could not find Summary of the main panel! \n Roblox error tree is below, it could be because you renamed the panel, or multiple UI's names 'panel' exist.")
 		end
-		if itmClass ~= "ModuleScript" then
-			local new = Frame.Template:Clone()
-			new.Visible = true
-			new.Parent = Frame
-			new.ItemName.Text = "loaded "..itmName
-			new.Type.Text = itmClass
-			if not GetTimeWithSeconds(Time) == "00:00:00" then
-				new.Time.Text = GetTimeWithSeconds(Time)
-			else
-				new.Time.Text = "🎉 Below 0"
-			end
-			new.Username.Text = plrLoaded.DisplayName.."(@"..plrLoaded.Name..")"
-			task.wait(Settings.renderTime)
-			new.thumbnail.Image = players:GetUserThumbnailAsync(plrLoaded.UserId, Enum.ThumbnailType.AvatarBust, Enum.ThumbnailSize.Size420x420)
+		if ItemClass ~= "ModuleScript" then
+			MakeHistory(
+				v,
+				players:GetUserThumbnailAsync(PlayerLoaded.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420),
+				PlayerLoaded.Name,
+				ItemName,
+				ItemClass,
+				GetTimeWithSeconds(Time)
+			)
 			local Home = Frame.Parent.Parent.AHome
 			Home.total.Text = GetShortNumer(#CompletedTimes).." total assets loaded"
 			Home.avg.Text = GetTimeWithSeconds(Average(CompletedTimes)).." average loading time, or lower"
 			Home.server.Text = GetShortNumer(ServerLifetime).." assets loaded in server lifetime"
-			print(Home.server.Text..", "..ServerLifetime)
 		else
 			table.insert(LoadedModules, item)
 			local new = Frame.Template:Clone()
 			new.Visible = true
 			new.Parent = Frame
-			new.ItemName.Text = "loaded "..itmName
-			new.Type.Text = itmClass
-			if not GetTimeWithSeconds(Time) == "00:00:00" then
+			new.ItemName.Text = "Loaded "..ItemName
+			new.Type.Text = ItemClass
+			if not (GetTimeWithSeconds(Time) == "00:00:00") then
 				new.Time.Text = GetTimeWithSeconds(Time)
 			else
 				new.Time.Text = "🎉 Below 0"
 			end
-			new.Username.Text = plrLoaded.DisplayName.."(@"..plrLoaded.Name..")"
+			new.Username.Text = PlayerLoaded.DisplayName.."(@"..PlayerLoaded.Name..")"
 			task.wait(Settings.renderTime)
-			new.thumbnail.Image = players:GetUserThumbnailAsync(plrLoaded.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+			new.thumbnail.Image = players:GetUserThumbnailAsync(PlayerLoaded.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+			--History
 			local new = Frame.Parent.Parent.Modules.MainFrame.Template:Clone()
 			new.Visible = true
 			new.Parent =  Frame.Parent.Parent.Modules.MainFrame
-			new.ItemName.Text = "loaded "..itmName
-			new.Type.Text = itmClass
+			new.ItemName.Text = "loaded "..ItemName
+			new.Type.Text = ItemClass
 			new.Time.Text = GetTimeWithSeconds(Time).." or lower"
-			new.Username.Text = plrLoaded.DisplayName.."(@"..plrLoaded.Name..")"
-			new.thumbnail.Image = players:GetUserThumbnailAsync(plrLoaded.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+			new.Username.Text = PlayerLoaded.DisplayName.."(@"..PlayerLoaded.Name..")"
+			new.thumbnail.Image = players:GetUserThumbnailAsync(PlayerLoaded.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
 			local Home = Frame.Parent.Parent.AHome
 			local ModulesFrame = Frame.Parent.Parent.Modules
 			Home.total.Text = GetShortNumer(#CompletedTimes).." total assets loaded"
---			Home.server.Text = GetShortNumer(ServerLifetime).." assets loaded in server lifetime"
---			print(Home.server.Text..", "..ServerLifetime)
-			if not GetTimeWithSeconds(Time) == "00:00:00" then
+			--			Home.server.Text = GetShortNumer(ServerLifetime).." assets loaded in server lifetime"
+			--			print(Home.server.Text..", "..ServerLifetime)
+			if not (GetTimeWithSeconds(Time) == "00:00:00") then
 				Home.avg.Text = GetTimeWithSeconds(Average(CompletedTimes)).." Average Loading Time"
 			else
 				Home.avg.Text = "🎉 Average is 0 or lower!"
 			end
 
-			if not #LoadedModules == 1 then
+			if not(#LoadedModules == 1) then
 				Home.modules.Text = GetShortNumer(#LoadedModules).." loaded modules"
 			else
 				Home.modules.Text = "1 loaded module"
